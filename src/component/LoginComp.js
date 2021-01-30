800bac9c48209166662767290b9767e6e1b5c84f
muhhylmi <hylmi.muh@gmail.com>
2021-01-30 13:30:52 +0700

    menambahkan component

---
+++ src/component/LoginComp.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function LoginComp() {
+  return <div></div>;
+}
+
+export default LoginComp;
---
+++ src/component/MenuComp.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MenuComp() {
+  return <div></div>;
+}
+
+export default MenuComp;
